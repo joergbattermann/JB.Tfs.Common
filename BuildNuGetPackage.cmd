0f0893fb63dbcde0c881514adffbce13a1ec0b2d
Joerg Battermann <jb@joergbattermann.com>
2012-01-28 14:02:22 +0100

    Adjusted NuGet spec and package to new namespace

--- BuildNuGetPackage.cmd
+++ BuildNuGetPackage.cmd
@@ -1,5 +1,5 @@
 @echo off
 if not exist .\Binaries mkdir .\Binaries
 if not exist .\Binaries\NuGet mkdir .\Binaries\NuGet
-.\Utils\nuget.exe pack -sym Tfs.Common.nuspec -o .\Binaries\NuGet
+.\Utils\nuget.exe pack -sym JB.Tfs.Common.nuspec -o .\Binaries\NuGet
 pause
