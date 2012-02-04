d95a38ba32d4b8491ab06730103fdfb0141f76fe
Joerg Battermann <jb@joergbattermann.com>
2012-02-04 15:10:32 +0100

    Async, more precisely TPL based Query methods added. Closes #6

---
+++ JB.Tfs.Common/QueryExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace JB.Tfs.Common
+{
+    /// <summary>
+    /// Extension Methods for <see cref="T:Microsoft.TeamFoundation.WorkItemTracking.Client.Query"/>
+    /// </summary>
+    public static class QueryExtensions
+    {
+        /// <summary>
+        /// Runs the query asynchronously.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<WorkItemCollection> RunQueryAsync(this Query query, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (query == null) throw new ArgumentNullException("query");
+            return Nito.AsyncEx.TeamFoundationClientAsyncFactory<WorkItemCollection>.FromApm(query.BeginQuery, query.EndQuery, cancellationToken);
+        }
+
+        /// <summary>
+        /// Runs the count query asynchronously.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<int> RunQueryCountAsync(this Query query, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (query == null) throw new ArgumentNullException("query");
+            return Nito.AsyncEx.TeamFoundationClientAsyncFactory<int>.FromApm(query.BeginCountOnlyQuery, query.EndCountOnlyQuery, cancellationToken);
+        }
+
+        /// <summary>
+        /// Runs the link query asynchronously.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<WorkItemLinkInfo[]> RunLinkQueryAsync(this Query query, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (query == null) throw new ArgumentNullException("query");
+            return Nito.AsyncEx.TeamFoundationClientAsyncFactory<WorkItemLinkInfo[]>.FromApm(query.BeginLinkQuery, query.EndLinkQuery, cancellationToken);
+        }
+    }
+}
--- JB.Tfs.Common/WorkItemStoreExtensions.cs
+++ JB.Tfs.Common/WorkItemStoreExtensions.cs
@@ -5,12 +5,18 @@
 // <author>Joerg Battermann</author>
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.TeamFoundation.WorkItemTracking.Client;
 
 namespace JB.Tfs.Common
 {
+    /// <summary>
+    /// Extension Methods for <see cref="T:Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItemStore"/>
+    /// </summary>
     public static class WorkItemStoreExtensions
     {
         /// <summary>
@@ -44,6 +50,118 @@ namespace JB.Tfs.Common
                 throw new ArgumentOutOfRangeException("globalListName", "A global list with this name does not exist in WorkItemStore");
 
             return globalList.Values ?? Enumerable.Empty<string>();
+        }
+
+        /// <summary>
+        /// Performs the Query asynchronously.
+        /// </summary>
+        /// <param name="workItemStore">The work item store.</param>
+        /// <param name="wiql">The wiql query.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<WorkItemCollection> QueryAsync(this WorkItemStore workItemStore, string wiql, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (workItemStore == null) throw new ArgumentNullException("workItemStore");
+            return new Query(workItemStore, wiql).RunQueryAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Performs the Query asynchronously.
+        /// </summary>
+        /// <param name="workItemStore">The work item store.</param>
+        /// <param name="wiql">The wiql.</param>
+        /// <param name="batchReadParameters">The batch read parameters.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<WorkItemCollection> QueryAsync(this WorkItemStore workItemStore, string wiql, BatchReadParameterCollection batchReadParameters, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (workItemStore == null) throw new ArgumentNullException("workItemStore");
+            return new Query(workItemStore, wiql, batchReadParameters).RunQueryAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Performs the Query asynchronously.
+        /// </summary>
+        /// <param name="workItemStore">The work item store.</param>
+        /// <param name="wiql">The wiql.</param>
+        /// <param name="ids">The ids.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<WorkItemCollection> QueryAsync(this WorkItemStore workItemStore, string wiql, int[] ids, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (workItemStore == null) throw new ArgumentNullException("workItemStore");
+            return new Query(workItemStore, wiql, ids).RunQueryAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Performs the Query asynchronously.
+        /// </summary>
+        /// <param name="workItemStore">The work item store.</param>
+        /// <param name="wiql">The wiql.</param>
+        /// <param name="ids">The ids.</param>
+        /// <param name="revs">The revs.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<WorkItemCollection> QueryAsync(this WorkItemStore workItemStore, string wiql, int[] ids, int[] revs, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (workItemStore == null) throw new ArgumentNullException("workItemStore");
+            return new Query(workItemStore, wiql, ids, revs).RunQueryAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Performs the Query asynchronously.
+        /// </summary>
+        /// <param name="workItemStore">The work item store.</param>
+        /// <param name="wiql">The wiql.</param>
+        /// <param name="context">The context.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<WorkItemCollection> QueryAsync(this WorkItemStore workItemStore, string wiql, IDictionary context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (workItemStore == null) throw new ArgumentNullException("workItemStore");
+            return new Query(workItemStore, wiql, context).RunQueryAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Performs the Query asynchronously.
+        /// </summary>
+        /// <param name="workItemStore">The work item store.</param>
+        /// <param name="wiql">The wiql.</param>
+        /// <param name="context">The context.</param>
+        /// <param name="dayPrevision">if set to <c>true</c> [day prevision].</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<WorkItemCollection> QueryAsync(this WorkItemStore workItemStore, string wiql, IDictionary context, bool dayPrevision, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (workItemStore == null) throw new ArgumentNullException("workItemStore");
+            return new Query(workItemStore, wiql, context, dayPrevision).RunQueryAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Performs the Count Query asynchronously.
+        /// </summary>
+        /// <param name="workItemStore">The work item store.</param>
+        /// <param name="wiql">The wiql.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<int> QueryCountAsync(this WorkItemStore workItemStore, string wiql, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (workItemStore == null) throw new ArgumentNullException("workItemStore");
+            return new Query(workItemStore, wiql).RunQueryCountAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Performs the Count Query asynchronously.
+        /// </summary>
+        /// <param name="workItemStore">The work item store.</param>
+        /// <param name="wiql">The wiql.</param>
+        /// <param name="context">The context.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        public static Task<int> QueryCountAsync(this WorkItemStore workItemStore, string wiql, IDictionary context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (workItemStore == null) throw new ArgumentNullException("workItemStore");
+            return new Query(workItemStore, wiql, context).RunQueryCountAsync(cancellationToken);
         }
     }
 }
